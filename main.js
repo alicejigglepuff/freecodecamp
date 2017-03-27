b9d96c4e68f0ca1fe8e1f1bfc5307ec95f4f2f98
Jiggly Puff <mian.wang.eit@gmail.com>
2017-03-27 07:11:57 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,44 @@
+
+//Setup
+var contacts = [
+    {
+        "firstName": "Akira",
+        "lastName": "Laine",
+        "number": "0543236543",
+        "likes": ["Pizza", "Coding", "Brownie Points"]
+    },
+    {
+        "firstName": "Harry",
+        "lastName": "Potter",
+        "number": "0994372684",
+        "likes": ["Hogwarts", "Magic", "Hagrid"]
+    },
+    {
+        "firstName": "Sherlock",
+        "lastName": "Holmes",
+        "number": "0487345643",
+        "likes": ["Intriguing Cases", "Violin"]
+    },
+    {
+        "firstName": "Kristian",
+        "lastName": "Vos",
+        "number": "unknown",
+        "likes": ["Javascript", "Gaming", "Foxes"]
+    }
+];
+
+
+function lookUpProfile(firstName, prop){
+// Only change code below this line
+  for (i = 0; i < contacts.length; i++) {
+    if (firstName == contacts[i].firstName) {
+      for (j = 0; j < contacts[i].length; j++){
+        if (prop == )
+      }
+    }
+  }
+// Only change code above this line
+}
+
+// Change these values to test your function
+lookUpProfile("Akira", "likes");
